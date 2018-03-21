b8ffea9921d79b7fefec751906667cc701bf61bd
sunainag <sunaina89.goyal@gmail.com>
2018-03-21 16:02:01 +0530

    First commit

---
+++ spring-core/src/main/java/com/sapient/spring_core/App.java
@@ -0,0 +1,13 @@
+package com.sapient.spring_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
