05883bc9944f9c0d258aaa0f5032d20a87f52603
sunainag <sunaina89.goyal@gmail.com>
2018-03-21 22:18:08 +0530

    collections as properties

--- spring-core/src/main/java/com/sapient/App.java
+++ spring-core/src/main/java/com/sapient/App.java
@@ -9,8 +9,32 @@ public class App
     public static void main( String[] args )
     {
     	AbstractApplicationContext  ctx = new ClassPathXmlApplicationContext("beans.xml");
-    	Student student = ctx.getBean(Student.class);
-    	student.printDetails();
+    	
+		/*
+		 * Student student = ctx.getBean(Student.class);
+		 * org.springframework.beans.factory.NoUniqueBeanDefinitionException: No
+		 * qualifying bean of type [com.sapient.model.Student] is defined: expected
+		 * single matching bean but found 2: student,scienceStudent
+		 * 
+		 * System.out.println(student.getName());
+		 * 
+		 * So use @Qualifier
+		 */
+    	
+    	Student student = (Student)ctx.getBean("student");
+    	
+    	System.out.println("*******printing list of address************");
+    	student.getAddressList().forEach(item->System.out.println(item));
+    	System.out.println("*******printing set of address*************");
+    	student.getAddressSet().forEach(System.out::println);
+    	System.out.println("*******printing map of address*************");
+    	student.getAddressMap().forEach((k,v)->System.out.println(k+","+v));
+    	System.out.println("*******printing properties of address*************");
+    	student.getAddressProp().forEach((k,v)->System.out.println(k+","+v));
+    	
+    	Student scienceStudent = (Student)ctx.getBean("scienceStudent");
+    	System.out.println("******student's name is ******\n"+student.getName());
+    	System.out.println("**********science student's name is *****\n"+scienceStudent.getName());
         ctx.registerShutdownHook();
     }
 }
--- spring-core/src/main/java/com/sapient/model/Student.java
+++ spring-core/src/main/java/com/sapient/model/Student.java
@@ -1,55 +1,54 @@
 package com.sapient.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class Student {
 	
 	private Integer age;
 	private String name;
-	private Address address;
-	
-	public Student() {
-		System.out.println("setter injection requires no-arg constructor");
-	}
-	
-	public Student(Integer age, String name, Address add) {
-		System.out.println("In argument constructor of student.");
-		this.age=age;
-		this.name=name;
-		this.address=add;
-	}
-
-
+	private List<String> addressList;
+	private Set<String> addressSet;
+	private Map<Integer, String> addressMap;
+	private Properties addressProp;
 	public Integer getAge() {
 		return age;
 	}
-
-
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-
-
 	public String getName() {
 		return name;
 	}
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
-	public Address getAddress() {
-		return address;
+	public List<String> getAddressList() {
+		return addressList;
 	}
-
-
-	public void setAddress(Address address) {
-		this.address = address;
+	public void setAddressList(List<String> addressList) {
+		this.addressList = addressList;
 	}
-
-	public void printDetails(){
-		System.out.println("student name: "+this.getName());
-		System.out.println("student age: "+this.getAge());
-		System.out.println("student address: "+this.getAddress());
+	public Set<String> getAddressSet() {
+		return addressSet;
 	}
+	public void setAddressSet(Set<String> addressSet) {
+		this.addressSet = addressSet;
+	}
+	public Map<Integer, String> getAddressMap() {
+		return addressMap;
+	}
+	public void setAddressMap(Map<Integer, String> addressMap) {
+		this.addressMap = addressMap;
+	}
+	public Properties getAddressProp() {
+		return addressProp;
+	}
+	public void setAddressProp(Properties addressProp) {
+		this.addressProp = addressProp;
+	}
+	
+
 }
