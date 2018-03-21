7466fdda84e8128400a93650808236328425f87c
sunainag <sunaina89.goyal@gmail.com>
2018-03-22 00:01:07 +0530

    Annotation-@Required

--- spring-core/src/main/java/com/sapient/App.java
+++ spring-core/src/main/java/com/sapient/App.java
@@ -9,13 +9,8 @@ public class App
     public static void main( String[] args )
     {
     	AbstractApplicationContext  ctx = new ClassPathXmlApplicationContext("beans.xml");
-    	System.out.println("******id:1, name:Sunaina ******");
     	Student one = (Student)ctx.getBean("studentOne");
-    	one.printDetails();
-    	System.out.println();
-    	System.out.println("*********id:2, name:Sam *****");
-    	Student two = (Student)ctx.getBean("studentTwo");
-    	two.printDetails();
+    	
         ctx.registerShutdownHook();
     }
 }
--- spring-core/src/main/java/com/sapient/model/Student.java
+++ spring-core/src/main/java/com/sapient/model/Student.java
@@ -1,28 +1,33 @@
 package com.sapient.model;
 
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Required;
 
 public class Student {
 
-	private int id;
+	private int age;
 	private String name;
-	private Map<Integer, String> idAddressMap;
 	
-	private Student(int id, String name) {
-		this.id=id;
-		this.name=name;
+	public int getAge() {
+		return age;
 	}
-	
-	public void setIdAddressMap(Map<Integer, String> idAddressMap) {
-		this.idAddressMap = idAddressMap;
+
+	@Required
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Required
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public void printDetails() {
 		System.out.println("name:"+this.name);
-		System.out.println("address:"+this.idAddressMap.get(this.id));
+		System.out.println("age:"+this.age);
 	}
 	
-	public static Student createStudentInstance(int id,String name) {
-		return new Student(id, name);
-	}
 }
