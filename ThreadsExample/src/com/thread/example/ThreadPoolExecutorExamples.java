0119a9b2fd141c3dd76251e73a96dd183bdd0b8b
sunainag <sunaina89.goyal@gmail.com>
2018-04-05 14:54:37 +0530

    ScheduledThreadPoolExecutor  - newScheduledThreadPool

--- ThreadsExample/src/com/thread/example/ThreadPoolExecutorExamples.java
+++ ThreadsExample/src/com/thread/example/ThreadPoolExecutorExamples.java
@@ -1,18 +1,24 @@
 package com.thread.example;
 
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecutorExamples {
 
-	/*
-	 * Schedule task with a delay
-	 */
-	public static void main(String[] args) throws InterruptedException, ExecutionException {ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
-	executor.schedule(() -> {
-	    System.out.println("Hello World");
-	}, 500, TimeUnit.MILLISECONDS);}
+	public static void main(String[] args) throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(3);
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
+		ScheduledFuture<?> future = executor.scheduleWithFixedDelay(() -> {
+			System.out.println(
+					" execute a task after 500 milliseconds delay and then repeat it every 100 milliseconds. After scheduling the task, we wait until it fires three times using the CountDownLatch lock, then cancel it using the Future.cancel() method");
+			latch.countDown();
+		}, 500, 100, TimeUnit.MILLISECONDS);
+
+		latch.await(1000,TimeUnit.MILLISECONDS);
+		future.cancel(true);
+	}
 
 }
