bbe8a36c98939a0ff88c7533ff95ca07448630d2
sunainag <sunaina89.goyal@gmail.com>
2018-04-05 14:45:14 +0530

    ScheduledThreadPoolExecutor

--- ThreadsExample/src/com/thread/example/ThreadPoolExecutorExamples.java
+++ ThreadsExample/src/com/thread/example/ThreadPoolExecutorExamples.java
@@ -1,30 +1,18 @@
 package com.thread.example;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecutorExamples {
 
 	/*
-	 * ThreadPoolExecutor is decorated with an immutable wrapper, so it cannot be
-	 * reconfigured after creation. Note that also this is the reason we cannot cast
-	 * it to a ThreadPoolExecutor.
+	 * Schedule task with a delay
 	 */
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-
-		AtomicInteger counter = new AtomicInteger();
-
-		ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
-		singleThreadExecutor.submit(() -> {
-			counter.set(1);
-		});
-		singleThreadExecutor.submit(() -> {
-			counter.compareAndSet(1, 2);
-		});
-
-		System.out.println(counter.get());
-	}
+	public static void main(String[] args) throws InterruptedException, ExecutionException {ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
+	executor.schedule(() -> {
+	    System.out.println("Hello World");
+	}, 500, TimeUnit.MILLISECONDS);}
 
 }
