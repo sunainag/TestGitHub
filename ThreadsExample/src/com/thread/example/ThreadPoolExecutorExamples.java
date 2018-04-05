5d78a71b17566c7140af0757d915665b1f061ef2
sunainag <sunaina89.goyal@gmail.com>
2018-04-05 13:34:28 +0530

    ExecutorService:submit a task ,Future‘s get()

--- ThreadsExample/src/com/thread/example/ThreadPoolExecutorExamples.java
+++ ThreadsExample/src/com/thread/example/ThreadPoolExecutorExamples.java
@@ -1,14 +1,21 @@
 package com.thread.example;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ThreadPoolExecutorExamples {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		Executor executor = Executors.newSingleThreadExecutor();
 		executor.execute(() -> System.out.println("Hello World"));
 
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		Future<String> future = executorService.submit(() -> "Hello World");
+		// some operations
+		String result = future.get();
 	}
 
 }
